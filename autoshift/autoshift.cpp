81d0d601d573eb8f0587da4e3560309c40ea1c81
Vincent Kosciuszko <vincentkosciuszko@outlook.fr>
2021-12-16 17:16:16 +0100

    Fixing indent level, use spaces over tabs

--- autoshift/autoshift.cpp
+++ autoshift/autoshift.cpp
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "constants.h"
 
-KeyState activekey {NULL, NULL};
+KeyState activekey{ NULL, NULL };
 
-
-int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance,
-    LPSTR lpCmdLine, int nCmdShow)
+INT WINAPI WinMain(
+    _In_ HINSTANCE hinstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPSTR lpCmdLine,
+    _In_ int nCmdShow)
 {
     HHOOK hhkLowLevelKybd = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, 0, 0);
     MSG msg;
@@ -28,38 +30,38 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
     {
         switch (wParam)
         {
-        case WM_KEYDOWN: 
+        case WM_KEYDOWN:
         case WM_SYSKEYDOWN:
-        case WM_KEYUP: 
+        case WM_KEYUP:
         case WM_SYSKEYUP:
             PKBDLLHOOKSTRUCT p = (PKBDLLHOOKSTRUCT)lParam;
             if (fEatKeystroke = IsInArray(keysToListen, p->vkCode))
             {
-				if (activekey.keyCode == NULL && ((wParam == WM_KEYDOWN) || (wParam == WM_SYSKEYDOWN)))	
+                if (activekey.keyCode == NULL && ((wParam == WM_KEYDOWN) || (wParam == WM_SYSKEYDOWN)))
                 {
                     activekey.keyCode = p->vkCode;
                     activekey.begin = clock();
-				}
-				else if (activekey.keyCode != NULL && ((wParam == WM_KEYUP) || (wParam == WM_SYSKEYUP)))
+                }
+                else if (activekey.keyCode != NULL && ((wParam == WM_KEYUP) || (wParam == WM_SYSKEYUP)))
                 {
                     clock_t end = clock();
                     double time_spent = ((double)end - (double)activekey.begin) / CLOCKS_PER_SEC;
                     if (time_spent > 0.175)
                     {
-						keybd_event(VK_CONTROL, 0, 0, 0);
-						keybd_event(VK_MENU, 0, 0, 0);
-						keybd_event(p->vkCode, 0, 0, 0);
-						keybd_event(p->vkCode, 0, KEYEVENTF_KEYUP, 0);
-						keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
-						keybd_event(VK_MENU, 0, KEYEVENTF_KEYUP, 0);
+                        keybd_event(VK_CONTROL, 0, 0, 0);
+                        keybd_event(VK_MENU, 0, 0, 0);
+                        keybd_event(p->vkCode, 0, 0, 0);
+                        keybd_event(p->vkCode, 0, KEYEVENTF_KEYUP, 0);
+                        keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
+                        keybd_event(VK_MENU, 0, KEYEVENTF_KEYUP, 0);
                     }
                     else {
-						keybd_event(p->vkCode, 0, 0, 0);
-						keybd_event(p->vkCode, 0, KEYEVENTF_KEYUP, 0);
+                        keybd_event(p->vkCode, 0, 0, 0);
+                        keybd_event(p->vkCode, 0, KEYEVENTF_KEYUP, 0);
                     }
                     activekey.begin = NULL;
                     activekey.keyCode = NULL;
-				}
+                }
             }
             break;
         }
